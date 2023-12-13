bc33638d24ca7b9efba1c4daf1f34f783e0aaf24
hugokermabon <hugo.kermabon@ensea.fr>
2023-12-13 17:08:55 +0100

    Step 3

--- TP2_step3.c
+++ TP2_step3.c
@@ -4,17 +4,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <netinet/in.h> 
-#include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
     // Verification of the number of arguments
     if (argc != 3) {
-        fprintf(stderr, "Required form: %s <host> <file>\n", argv[0]);
+        fprintf(stderr, "Required_form: %s <host> <file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    //Extraction of request arguments
+    // Extraction of request arguments
     char *host = argv[1];
     char *file = argv[2];
 
@@ -22,49 +20,48 @@ int main(int argc, char *argv[]) {
     printf("Host: %s\n", host);
     printf("File: %s\n", file);
 
-    // Creating a UDP socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // Creation of the socket
+    int sockfd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+    
+    //Error test
     if (sockfd == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
-    }
-
-    struct addrinfo hints, *result, *p;
+    }   
+    
+    struct addrinfo hints, *result;
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC; // AF_UNSPEC allows both IPv4 and IPv6
     hints.ai_socktype = SOCK_DGRAM; // Use SOCK_DGRAM for UDP
 
     int status = getaddrinfo(host, NULL, &hints, &result);
     if (status != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        close(sockfd); // Close the socket before exiting
+        fprintf(stderr, "Error resolving host: %s\n", gai_strerror(status));
         exit(EXIT_FAILURE);
     }
 
-    // Iterate through the list of addresses and print information
+    // Display the resolved IP addresses
+    struct addrinfo *p;
     for (p = result; p != NULL; p = p->ai_next) {
-        char addr[INET6_ADDRSTRLEN];
+        void *addr;
+        char ipstr[INET6_ADDRSTRLEN];
 
-        // Print the IP address
-        void *ipaddr;
         if (p->ai_family == AF_INET) {
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            ipaddr = &(ipv4->sin_addr);
+            addr = &(ipv4->sin_addr);
         } else {
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-            ipaddr = &(ipv6->sin6_addr);
+            addr = &(ipv6->sin6_addr);
         }
 
-        // Convert the IP address to a human-readable form
-        inet_ntop(p->ai_family, ipaddr, addr, sizeof(addr));
-        printf("IP Address: %s\n", addr);
+        // Convert the IP address to a human-readable string
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        printf("Resolved IP: %s\n", ipstr);
     }
-
-    // Free the memory allocated by getaddrinfo
-    freeaddrinfo(result);
-
-    // Close the socket
-    close(sockfd);
-
+    // Connect to the server
+    
+}
+    freeaddrinfo(result); // Free the memory allocated by getaddrinfo
+    freeaddrinfo(server_info); // Free the memory allocated by getaddrinfo 
     return 0;
 }
